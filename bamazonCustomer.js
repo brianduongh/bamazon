79bb45a08923f99dcf9fb0cb80e8b71ed591f4ac
Brian Duong <brianduongh@gmail.com>
2018-11-28 23:28:54 -0600

    added inquery and purchase feature

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
+// Connect to mysql database
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -11,29 +13,84 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log('connected as id ' + connection.threadId);
-  afterConnection();
+  console.log('WELCOME TO BAMAZON');
+  printInventory();
+  // afterConnection();
 });
 
-const afterConnection = () => {
-  connection.query("SELECT * FROM products", function(err, res) {
+
+
+
+// Prints inventory
+const printInventory = () => {
+  console.log('Printing Inventory...');
+  let query = connection.query(
+    'SELECT * FROM products',
+    function(err,res) {
     if (err) throw err;
-    console.log(res);
-    // console.log("---WELCOME TO BAMAZON---");
-    // res.forEach(product => {
-    //   console.log(product.id);
-    // })
-    printProducts(res);
-    connection.end();
+    res.forEach(product => {
+      console.log(`${product.id}\t${product.product_name}\t${product.price}\t${product.stock_quantity}`)
+    });
+    purchaseItem();
   });
 };
 
-const printProducts = products => {
-  console.log(
-`
---- WELCOME TO BAMAZON ---
-`
+const purchaseItem = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What product id would you like to buy?'
+    },
+    {
+      type: 'input',
+      name: 'amt',
+      message: 'How much would you like to buy?'
+    }
+  ]).then(function(res) {
+    updateInventory(res.id, res.amt);
+  });
+};
+
+const updateInventory = (id, amount) => {
+  let query = connection.query(
+    'SELECT * FROM products WHERE ?',
+    [
+      {
+        id: id
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      let stockQuantity = res[0].stock_quantity;
+      let stockLeft = stockQuantity - amount;
+      let total = res[0].price * amount;
+      if (stockQuantity > 0) {
+        updateStock(id, stockLeft, total);
+      } else {
+        console.log("There's not enough items!");
+        purchaseItem();
+      };
+    }
+  )
+};
+
+const updateStock = (id, stockLeft, total) => {
+  let query = connection.query(
+    'UPDATE products SET ? WHERE ?',
+    [
+      {
+        stock_quantity: stockLeft
+      },
+      {
+        id: id
+      }
+    ],
+    function(err) {
+      if (err) throw err;
+      console.log("You have purchased an item");
+      console.log("Your total is: " + total);
+      printInventory();
+    }
   );
-  products.forEach(product => {
-    console.log(product.product_name);
-  })
 };
